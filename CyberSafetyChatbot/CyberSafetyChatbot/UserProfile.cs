b43f26ee5eaf9f1329f0a821b4f69dfa379e87f4
Nokuthula-coder <ST10499051@rcconnect.edu.za>
2026-09-10 16:27:22 +0200

    Added User Profile with name property for personali

---
+++ CyberSafetyChatbot/CyberSafetyChatbot/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CyberSafetyChatbot
+{
+    internal class UserProfile
+    {
+            // Properties to store user information
+            public string Name { get; set; } = "";
+        }
+    }
+
+
