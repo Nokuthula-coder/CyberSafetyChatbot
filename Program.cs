39fb66764e5bc714db46952f362cb2f10667952d
Nokuthula-coder <ST10499051@rcconnect.edu.za>
2026-09-10 21:00:07 +0200

    Initial commit: Set up project structure and main files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CyberSafetyChatbot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Front door of application
+            // its job is to create your chatbot object  and start the program
+
+            Chatbot bot = new Chatbot();
+
+        // Start the chatbot
+        bot.Start();
+
+        }
+    }
+}
+   
